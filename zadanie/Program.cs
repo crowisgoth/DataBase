8eb55cd691fa802af2f16afdbe88decbcde17c80
Ваня Борисов <crowisgoth@vk.com>
2023-08-13 16:04:51 +0300

    Починил файл Program.cs

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -4,201 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Npgsql;
+using DataBaseAction;
+using Output;
+using sqlq;
+using Menu;
 
-namespace zadanie
+namespace WorkStation
 {
+    
     class Program
     {
-
+        
+        ConsoleOutput op = new ConsoleOutput();
+        SQLq sql = new SQLq();
+        
         static void Main(string[] args)
         {
-            Program create = new Program();
-            string connString = "Host=185.112.83.196;Username=postgres;Password=mysecretpassword;Database=postgres";
-            Console.WriteLine("Меню\n Введите необходимую цифру \n 1 - Cоздать таблицу\n 2 - Проверить соединение \n 3 - Добавить записи в таблицу \n 4 - Изменить записи \n 5 - Удалить запись\n 6 - Закрыть программу \n 7 - Вывести таблицу");
-            int Num = int.Parse(Console.ReadLine());
-
-            do
-            {
-
-
-                switch (Num)
-                {
-                    case 1:
-                        create.CreateTable(connString);
-                        break;
-                    case 2:
-                        ConnTry(connString);
-                        break;
-                    case 3:
-                        create.AddInfo(connString);
-                        break;
-                    case 4:
-                        create.InsertInfo(connString);
-                        break;
-                    case 5:
-                        create.deleteInfo(connString);
-                        break;
-                    case 6:
-                        break;
-                    case 7:
-                        create.readInfo(connString);
-                        break;
-
-                }
-                Console.WriteLine("Меню\n Введите необходимую цифру \n 1 - Cоздать таблицу\n 2 - Проверить соединение \n 3 - Добавить записи в таблицу \n 4 - Изменить записи \n5 - Удалить запись\n 6 - Закрыть программу \n7 - Вывести таблицу");
-                Num = int.Parse(Console.ReadLine());
-            }
-            while (Num != 6);
-
-
-
-
-
-
+            
+           
+            mainMenu(connInfo());
+           
 
         }
-
-        static void ConnTry(string connString)
+        public static void mainMenu(string connString)
+        {
+            MyMenu menu = new MyMenu();
+            menu.MainMenu(connString);
+           
+        }
+        static string connInfo()
+        {
+            string connString = "Host=" + DataBaseAction.Class1.Host + ";" + "Username=" + DataBaseAction.Class1.Username + ";" + "Password=" + DataBaseAction.Class1.Password + ";" + "Database=" + DataBaseAction.Class1.Database;
+            return connString;
+        }
+        static NpgsqlCommand Connection(string connString, string sqlQuery)
         {
             NpgsqlConnection nc = new NpgsqlConnection(connString);
+            NpgsqlCommand execQuery = new NpgsqlCommand(sqlQuery, nc);
             try
             {
-                //Открываем соединение.
                 nc.Open();
-                Console.WriteLine("Подключение к серверу прошло успешно");
-                Console.ReadLine();
-                nc.Close();
+                execQuery.ExecuteNonQuery();
+                return execQuery;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Ошибка при подключении к серверу");
+                Console.WriteLine(ex);
+                return execQuery;
                 Console.ReadLine();
             }
         }
+
         public void CreateTable(string connString)
         {
-            NpgsqlConnection nc = new NpgsqlConnection(connString);
-            try
-            {
-                nc.Open();
-                string sql = "CREATE TABLE Users (id serial primary key,created_by character varying(30),updated_by character varying(30),first_name text,last_name text,age integer,mail text);";
-                NpgsqlCommand ng = new NpgsqlCommand(sql, nc);
-                ng.ExecuteNonQuery();
-
-                Console.WriteLine("Запрос отправлен");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Произошла ошибка при попытке выполнения запроса (Таблица уже создана)");
-            }
-
-
-            nc.Close();
-
+            
+            Connection(connString,sql.createTableSQL());
+            op.createTableOutput();
         }
         public void AddInfo(string connString)
         {
-            Console.WriteLine("Введите ваше имя");
-            string first_name = Console.ReadLine();
-            Console.WriteLine("Введите вашу фамилию");
-            string last_name = Console.ReadLine();
-            Console.WriteLine("Введите ваш возраст");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите ваш адрес электронной почты");
-            string mail = Console.ReadLine();
-            NpgsqlConnection nc = new NpgsqlConnection(connString);
-            try
-            {
-                Console.WriteLine("Давайте заполним ваши данные");
-
-                nc.Open();
-                string sql = "INSERT INTO Users (created_by,first_name,last_name,age,mail) VALUES ('" + DateTime.Now + "','" + first_name + "','" + last_name + "'," + age + ",'" + mail + "');";
-                //string sql = "INSERT INTO Users (created_by) VALUES ('" + @DateTime.Now + "');";
-                //NpgsqlParameter nameparam = new NpgsqlParameter("'@DateTime.Now'", @DateTime.Now);
-                NpgsqlCommand cmd = new NpgsqlCommand();
-                //cmd.Parameters.Add(nameparam);
-                NpgsqlCommand com = new NpgsqlCommand(sql, nc);
-                com.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-
-
-
-            nc.Close();
-
+            
+            Connection(connString, sql.addInfoSQL(op.AddInfoOutput()));
         }
         public void InsertInfo(string connString)
         {
-            Console.WriteLine("Введите имя поля, которое вы хотите изменить");
-            string changedName = Console.ReadLine();
-            Console.WriteLine("Введите ID нужного поля");
-            int changedID = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите значение");
-            string newValue = Console.ReadLine();
-            try
-            {
-                NpgsqlConnection nc = new NpgsqlConnection(connString);
-                nc.Open();
-                string sql = "UPDATE users SET " + changedName + "=" + "'" + newValue + "'" + ";";
-                NpgsqlCommand com = new NpgsqlCommand(sql, nc);
 
-                com.ExecuteNonQuery();
-                nc.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
+            Connection(connString, sql.insertInfoSQL(op.insertInfoPath()));
         }
 
-        public void deleteInfo(string ConnString)
+        public void deleteInfo(string connString)
         {
-            Console.WriteLine("Введите ID записи которую хотите удалить");
-            int NumDelete = int.Parse(Console.ReadLine());
-            try
-            {
-                NpgsqlConnection nc = new NpgsqlConnection(ConnString);
-                nc.Open();
-                string sql = "DELETE FROM users WHERE id = " + "'" + NumDelete + "'" + ";";
-                NpgsqlCommand com = new NpgsqlCommand(sql, nc);
-                com.ExecuteNonQuery();
-                nc.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
 
+            Connection(connString, sql.deleteInfoSQL());
         }
-        public void readInfo(string ConnString)
+        public void readInfo(string connString)
         {
-            NpgsqlConnection nc = new NpgsqlConnection(ConnString);
-            nc.Open();
-            string strSQL = "SELECT * FROM users";
-            NpgsqlCommand myCommand = new NpgsqlCommand(strSQL, nc);
-            NpgsqlDataReader dr = myCommand.ExecuteReader();
-        
-            int iq = 3;
+            
+            NpgsqlDataReader dr = Connection(connString, sql.readInfoSQL()).ExecuteReader();
             while (dr.Read())
             {
-
                 object id = dr.GetValue(1);
                 object fistName = dr.GetValue(3);
                 object secName = dr.GetValue(4);
-                //object age = dr.GetValue(5);
-                //object mail = dr.GetValue(6);
                 Console.WriteLine("{0} \t{1}\t{2}", id, fistName, secName);
-                
             }
-
             dr.Close();
-
-
         }
+
+        
     }
-    }
+}
