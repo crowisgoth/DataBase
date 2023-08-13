5e37037d1395371ab4d91efedaa4513fc333b921
Ваня Борисов <crowisgoth@vk.com>
2023-08-13 15:48:06 +0300

    расширил функционал

---
+++ zadanie/ConsoleOutput.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output
+{
+    class ConsoleOutput
+    {
+        public void createTableOutput()
+        {
+
+            Console.WriteLine("Запрос отправлен");
+        }
+        public string inputConsole(string text)
+        {
+
+            Console.WriteLine(text);
+            return Console.ReadLine();
+        }
+        public int deletedID()
+        {
+            Console.WriteLine("Введите ID записи которую хотите удалить");
+            int delID =int.Parse(Console.ReadLine());
+            return delID;
+        }
+        public Dictionary<string, string> AddInfoOutput()
+        {
+            Dictionary<string, string> nums = new Dictionary<string, string>();
+            nums.Add("fName", inputConsole("Введите ваше имя"));
+            nums.Add("sName", inputConsole("Введите вашу фамилию"));
+            while (true) { 
+                try
+                {
+                    int age = int.Parse(inputConsole("Введите ваш возраст"));
+                    nums.Add("age", age.ToString());
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Проверьте введенные данные");
+
+                }
+
+            }
+
+            nums.Add("mail", inputConsole("Введите ваш адрес электронной почты"));
+
+            return nums;
+        }
+        public Dictionary<string,string> insertInfoPath()
+        {
+            Dictionary<string, string> changedInfo = new Dictionary<string, string>();
+            changedInfo.Add("changedName", inputConsole("Введите имя поля которое хотите изменить"));
+            try
+            {
+                int id = int.Parse(inputConsole("Введите ID элемента который хотите изменить"));
+                changedInfo.Add("ID", id.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Проверьте введенные данные");
+            }
+
+            changedInfo.Add("changedValue", inputConsole("Введите новое значение"));
+            return changedInfo;
+        }
+        
+            
+    }
+}
---
+++ zadanie/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkStation;
+
+namespace Menu
+{
+    class MyMenu
+    {
+        Program conn = new Program();
+        
+       
+        public void MainMenu(string connString)
+        {
+            Console.WriteLine("Меню\n Введите необходимую цифру \n 1 - Cоздать таблицу\n  2 - Добавить записи в таблицу \n 3 - Изменить записи \n 4 - Удалить запись\n 5 - Закрыть программу \n 6 - Вывести таблицу");
+            int Num = int.Parse(Console.ReadLine());
+            //ss
+            do
+            {
+                switch (Num)
+                {
+                    case 1:
+                        conn.CreateTable(connString);
+                        break;
+                    case 2:
+                        conn.AddInfo(connString);
+                        break;
+                    case 3:
+                        conn.InsertInfo(connString);
+                        break;
+                    case 4:
+                        conn.deleteInfo(connString);
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        conn.readInfo(connString);
+                        break;
+                }
+                Console.WriteLine("Меню\n Введите необходимую цифру \n 1 - Cоздать таблицу\n  2 - Добавить записи в таблицу \n 3 - Изменить записи \n4 - Удалить запись\n 5 - Закрыть программу \n6 - Вывести таблицу");
+                Num = int.Parse(Console.ReadLine());
+            }
+            while (Num != 5);
+        }
+    }
+}
